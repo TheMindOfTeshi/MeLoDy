7ed61cff098f9308db54cf5717c78b09bf9ad3fc
Teshi <moritz.johannes.bauer@gmail.com>
2020-05-25 23:51:23 +0200

    Added Respawn Trigger Script
    used for putting the PlayerCharacter back to it's spawn location, when it enters the Collider

---
+++ Assets/RespawnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RespawnTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnLocation;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnLocation.transform.position;
+    }
+
+}
